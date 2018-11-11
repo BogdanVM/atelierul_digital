f19b0d4a4e2cb26871690f84074804a91705ae80
BogdanVM <bogdanvm98@gmail.com>
2018-11-11 16:48:03 +0200

    Update FizzBuzz.java

--- Curs 1/FizzBuzz/1/src/FizzBuzz.java
+++ Curs 1/FizzBuzz/1/src/FizzBuzz.java
@@ -6,23 +6,27 @@ public class FizzBuzz {
     public FizzBuzz(int _numberOfElements) {
 
         numberOfElements = _numberOfElements;
-        codifications = new PairFizzBuzz[0];
+        codifications = new PairFizzBuzz[0]; // at first there is no codification
     }
 
     public void addCodification(int position, String newCodification) {
 
+        // create an auxiliar array where we add the new pair
         PairFizzBuzz[] auxArray = new PairFizzBuzz[codifications.length + 1];
         System.arraycopy(codifications, 0, auxArray, 0, codifications.length);
         auxArray[auxArray.length - 1] = new PairFizzBuzz(position, newCodification);
 
+        // we save the changes in the codifications array
         codifications = auxArray;
     }
 
     public String getCode(int position) {
 
-        String code = "";
+        String code = ""; // at first there is no codification at the current position
         for (int i = 0; i < codifications.length; ++i) {
             if (position % codifications[i].getNumber() == 0) {
+                
+                // if the position is divisible to the codified number, we add its codification to the final code
                 code = code + codifications[i].getCode();
             }
         }
